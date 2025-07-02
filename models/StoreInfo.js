7ff23b682bff2f2ae6e8968dafeafd1e43bb35e8
cgarcia1196 <57427684+cgarcia1196@users.noreply.github.com>
2025-07-02 16:02:11 -0700

    fixed bug: when editing menu items certain items would not update (#15)

--- models/StoreInfo.js
+++ models/StoreInfo.js
@@ -1,32 +1,5 @@
 const mongoose = require('mongoose');
 
-const storeInfoSchema = new mongoose.Schema({
-  location: String,
-  phone: String,
-  hours: {
-    mon: String,
-    tue: String,
-    wed: String,
-    thu: String,
-    fri: String,
-    sat: String,
-    sun: String
-  },
-  apple_maps: String,
-  google_maps: String,
-  holidayNotices: [
-    {
-      type: { type: String, enum: ['open', 'closed'] },
-      message: String,
-      effectiveDate: Date,
-      showFrom: Date
-    }
-  ],
-  lastEditedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  updatedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('StoreInfo', storeInfoSchema);
 const StoreInfoSchema = new mongoose.Schema({
   storeName: {
     type: String,
