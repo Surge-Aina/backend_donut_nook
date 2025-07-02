87c338cdde87631beb961d0bb7a90d2170ea20b8
Sathvik Thatikonda <sathvi.thatikonda@gmail.com>
2025-07-02 14:58:03 -0500

    Updated index.js to finalize merge resolution

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./utils/db');
 const testRoute = require('./routes/testRoute');
-const userRoutes = require('./routes/users'); // ✅ add this line
+const userRoutes = require('./routes/users');
 const storeRoutes = require('./routes/storeRoutes');
 const aboutRoutes = require('./routes/aboutRoutes');
 const menuRoutes = require('./routes/menu');
-const holidayRoutes = require('./routes/holidayRoutes');\
 const storeInfoRoutes = require('./routes/storeInfoRoutes');
-
+const holidayRoutes = require('./routes/holidayRoutes'); // ✅ Added line
 
 const app = express();
 
@@ -18,15 +17,12 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
 app.use('/test', testRoute);
-
-app.use('/users', userRoutes); // ✅ mount route prefix
-
+app.use('/users', userRoutes);
 app.use('/api/store', storeRoutes);
 app.use('/api/about', aboutRoutes);
 app.use('/menu', menuRoutes);
-app.use('/api/holidays', holidayRoutes);
-// Add store info routes
 app.use('/api/store-info', storeInfoRoutes);
+app.use('/api/holidays', holidayRoutes); // ✅ Added line
 
 app.get('/', (req, res) => {
   res.send('🍩 Donut Nook Backend is Alive!');
